f0aeccb478444bdecb8d0a7d3b9df916a0deaf2f
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-11-09 08:56:04 -0400

    primer test ccon cypress

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,9 +1,26 @@
 describe("Sumador", () => {
   it("Shows the amount of the addition to the user", () => {
+    //arrange 
     cy.visit("/");
     cy.get("#primer-numero").type(4);
     cy.get("#segundo-numero").type(5);
+    //act
     cy.get("#sumar-button").click();
+    //asser(verificar)
     cy.get("#resultado-div").should("contain", "9");
   });
+
+  it("Shows the amount of the addition to the user", () => {
+    //arrange 
+    cy.visit("/");
+    cy.get("#primer-numero").type(7);
+    cy.get("#segundo-numero").type(7);
+    //act
+    cy.get("#sumar-button").click();
+    //asser(verificar)
+    cy.get("#resultado-div").should("contain", "14");
+  });
+
+
+
 });
